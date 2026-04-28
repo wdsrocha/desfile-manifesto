7bcc463d97fd8c9e29904f28f878175ff2e65f84
Wesley Rocha <wesleysr1997@gmail.com>
2026-04-28 02:37:19 -0400

    chore: remove credits data from lib/data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -15,16 +15,6 @@ export interface DesfileFollowUp {
   local: string;
 }
 
-export interface CreditoPessoa {
-  nome?: string;
-  instagram?: string;
-}
-
-export interface CreditoGrupo {
-  titulo: string;
-  pessoas: CreditoPessoa[];
-}
-
 export interface EventoInfo {
   nome: string;
   edicao: string;
@@ -60,56 +50,3 @@ export const proximoDesfile: DesfileFollowUp = {
 
 export const looks: Look[] = [];
 
-export const creditos: CreditoGrupo[] = [
-  {
-    titulo: "Elenco",
-    pessoas: [
-      { instagram: "@travamazonica" },
-      { instagram: "@art.delly" },
-      { instagram: "@biolly.shantelly" },
-      { instagram: "@viquizando" },
-      { instagram: "@subprodut0" },
-      { instagram: "@oadriel_castro" },
-      { instagram: "@dona_navra" },
-      { instagram: "@ariskaderi" },
-      { instagram: "@dacotamc" },
-      { instagram: "@manucarihuazari" },
-      { instagram: "@anlyds" },
-      { instagram: "@krait_mandaca" },
-      { instagram: "@nataliapovoass" },
-      { instagram: "@larinha_numbb" },
-      { instagram: "@menormc1" },
-    ],
-  },
-  {
-    titulo: "Equipe voluntária",
-    pessoas: [
-      { instagram: "@eumayaalves" },
-      { instagram: "@_sou_hi" },
-      { instagram: "@lylleabreu" },
-      { instagram: "@modatbmpensa" },
-      { instagram: "@dione.maciel.90" },
-      { instagram: "@holandapaulo" },
-      { instagram: "@sharp.freestyle" },
-      { nome: "Daina Viana" },
-    ],
-  },
-  {
-    titulo: "Fotógrafos",
-    pessoas: [
-      { instagram: "@cavalcantte" },
-      { instagram: "@henrifotografo" },
-    ],
-  },
-  {
-    titulo: "Apoio institucional",
-    pessoas: [
-      { instagram: "@manauscult_" },
-      { instagram: "@semjelmanaus" },
-    ],
-  },
-  {
-    titulo: "Parceiros oficiais",
-    pessoas: [{ instagram: "@prestigehotelmanaus" }],
-  },
-];
