dfd20aa76b8fa6732d8e629c6669e19c2c5689a8
Wesley Rocha <wesleysr1997@gmail.com>
2026-05-02 12:42:59 -0400

    feat(looks): project images array in groq query

--- src/sanity/queries/looks.ts
+++ src/sanity/queries/looks.ts
@@ -4,7 +4,12 @@ export const allLooksQuery = defineQuery(`
   *[_type == "look"] | order(lookNumber asc) {
     _id,
     lookNumber,
-    image,
+    images[]{
+      asset,
+      alt,
+      photographerName,
+      photographerInstagram
+    },
     model {
       name,
       instagram
